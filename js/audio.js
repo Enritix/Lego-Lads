c3f6b8d21fbb655fcdb8fc0df031c2bf167cec2e
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-02-19 12:11:00 +0100

    add-mobile-nav

---
+++ js/audio.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sound = document.getElementById("clickSound");
+    const musicButton = document.getElementById("music-toggle");
+    const muteLine = document.getElementById("mute-line");
+
+    let soundEnabled = true;
+
+    if (musicButton) {
+        musicButton.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            soundEnabled = !soundEnabled;
+            muteLine.style.display = soundEnabled ? "none" : "block";
+        });
+    }
+    if (sound) {
+        document.addEventListener("click", function (event) {
+            if (soundEnabled) {
+                sound.currentTime = 0;
+                sound.play().catch(error => console.error("Audio kon nit worden afgespeeld:", error));
+            }
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger-menu");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        if (navMenu.style.display === "block") {
+            navMenu.style.display = "none";
+        } else {
+            navMenu.style.display = "block";
+        }
+    });
+});
