12cc2ab69faf8abf8588a67d1bb13f437e76fbf0
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-03-10 18:37:39 +0100

    change-reason functionaliteit

--- js/blacklist.js
+++ js/blacklist.js
@@ -1,25 +1,23 @@
-document.querySelectorAll('.remove-btn').forEach(button => {
-    button.addEventListener('click', function () {
-        this.parentElement.remove();
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-    const infoIcon = document.querySelector('.info-icon');
+    document.querySelectorAll('.remove-btn').forEach(button => {
+      button.addEventListener('click', function() {
+        this.parentElement.remove();
+      });
+    });
+  
     const infoPopup = document.getElementById('info-popup');
-    const closeButton = document.querySelector('.close-button');
-
-    infoIcon.addEventListener('click', function() {
-        infoPopup.style.display = 'flex';
+    document.querySelector('.info-icon').addEventListener('click', () => {
+      infoPopup.style.display = 'flex';
     });
-
-    closeButton.addEventListener('click', function() {
+  
+    document.querySelector('.close-button').addEventListener('click', () => {
+      infoPopup.style.display = 'none';
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target === infoPopup) {
         infoPopup.style.display = 'none';
+      }
     });
-
-    window.addEventListener('click', function(event) {
-        if (event.target === infoPopup) {
-            infoPopup.style.display = 'none';
-        }
-    });
-});
+  });
+  
