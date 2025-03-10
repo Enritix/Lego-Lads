c45833619cfcbaf117f8f7ddda532b1d250d17b1
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-03-10 17:46:34 +0100

    change-pop up verbetering

--- js/blacklist.js
+++ js/blacklist.js
@@ -9,17 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const infoPopup = document.getElementById('info-popup');
     const closeButton = document.querySelector('.close-button');
 
-    // Open de popup wanneer op het info icoon wordt geklikt
     infoIcon.addEventListener('click', function() {
-        infoPopup.style.display = 'flex'; // Gebruik 'flex' om te centreren
+        infoPopup.style.display = 'flex';
     });
 
-    // Sluit de popup wanneer op de sluitknop wordt geklikt
     closeButton.addEventListener('click', function() {
         infoPopup.style.display = 'none';
     });
 
-    // Sluit de popup wanneer er buiten de popup wordt geklikt
     window.addEventListener('click', function(event) {
         if (event.target === infoPopup) {
             infoPopup.style.display = 'none';
