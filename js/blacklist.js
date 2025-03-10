02ae11aed4093c13ae3ad10d0a7809cd1d7ac5e1
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-03-10 17:21:13 +0100

    change-responsivity

--- js/blacklist.js
+++ js/blacklist.js
@@ -3,3 +3,26 @@ document.querySelectorAll('.remove-btn').forEach(button => {
         this.parentElement.remove();
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const infoIcon = document.querySelector('.info-icon');
+    const infoPopup = document.getElementById('info-popup');
+    const closeButton = document.querySelector('.close-button');
+
+    // Open de popup wanneer op het info icoon wordt geklikt
+    infoIcon.addEventListener('click', function() {
+        infoPopup.style.display = 'flex'; // Gebruik 'flex' om te centreren
+    });
+
+    // Sluit de popup wanneer op de sluitknop wordt geklikt
+    closeButton.addEventListener('click', function() {
+        infoPopup.style.display = 'none';
+    });
+
+    // Sluit de popup wanneer er buiten de popup wordt geklikt
+    window.addEventListener('click', function(event) {
+        if (event.target === infoPopup) {
+            infoPopup.style.display = 'none';
+        }
+    });
+});
