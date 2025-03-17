8a16b7e9e425b71d8c0891af3406d0417a64d375
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-03-17 20:39:39 +0100

    change-responsivity

--- js/genre.js
+++ js/genre.js
@@ -53,4 +53,25 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   updateCarousel();
+
+  const infoIcon = document.querySelector('.info-icon');
+  const popup = document.querySelector('.popup');
+  const closeButton = document.querySelector('.close-button');
+  
+  infoIcon.addEventListener('click', () => {
+    popup.style.display = 'flex';
+  });
+  
+  closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+  
+  window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+  
+  
 });
+
