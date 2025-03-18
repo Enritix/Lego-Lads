457007ef41fb873c9523c93141f5a9933d6c7ccb
Enritix <enrico.egghe@hotmail.com>
2025-03-18 21:27:17 +0100

    factory-page mobile gemaakt

--- js/factory.js
+++ js/factory.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const minifigures = document.getElementById("minifigures");
-    const gears = document.querySelectorAll(".factory-belt-gear");
+    const minifigures = document.getElementById("minifigures-desktop");
+    const gears = document.querySelectorAll(".factory-belt-gear-desktop");
 
     minifigures.addEventListener("animationend", () => {
         gears.forEach(gear => {
+            gear.style.animation = "none";
+        });
+    });
+
+    const minifiguresMobile = document.getElementById("minifigures-mobile");
+    const gearsMobile = document.querySelectorAll(".factory-belt-gear-mobile");
+
+    minifiguresMobile.addEventListener("animationend", () => {
+        gearsMobile.forEach(gear => {
             gear.style.animation = "none";
         });
     });
