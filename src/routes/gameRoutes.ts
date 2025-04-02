5158630b67b4a407e785bd3201a7db507029e478
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-04-02 10:47:44 +0000

    add-ejs

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 const router = express.Router();
 
-router.get('/clickergame', (req: Request, res: Response) => {
-  res.render('clickergame');
+router.get("/clickergame", (req: Request, res: Response) => {
+  res.render("clickergame");
 });
 
-router.get('/memorygame', (req: Request, res: Response) => {
-  res.render('memorygame');
+router.get("/memorygame", (req: Request, res: Response) => {
+  res.render("memorygame");
 });
 
 export default router;
