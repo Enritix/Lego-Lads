09c93d82463a0ec79cd2899fe05c7f5f209737a5
Enrico Egghe <enrico.egghe@student.kdg.be>
2025-05-07 07:34:55 +0000

    added interface for user

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,78 @@
-export interface Achievement {
+// export interface Achievement {
 
-}
+// }
+
+interface User {
+    _id: {
+      $oid: string;
+    };
+    username: string;
+    password: string;
+    email: string;
+    profile_fig: string;
+    coins: number;
+    earned_coins: number;
+    spent_coins: number;
+    figs: {
+      name: string;
+      img: string;
+      rarity: string;
+    }[];
+    bin: {
+      fig: string;
+      reason: string;
+    }[];
+    ordenedFigs: {
+      fig: string;
+      set: string;
+      theme: string;
+    }[];
+    achievements: {
+      coins: {
+        title: string;
+        description: string;
+        current: number;
+        goal: number;
+        reward: number;
+        finished: boolean;
+        collected: boolean;
+      };
+      login_streak: {
+        title: string;
+        description: string;
+        current: number;
+        goal: number;
+        reward: number;
+        finished: boolean;
+        collected: boolean;
+      };
+    };
+    chests: {
+      uncommon: number;
+      epic: number;
+      legendary: number;
+    };
+    keys: number;
+    clickerGame: {
+      blocks: number;
+      tools: {
+        hammer: {
+          level: number;
+        };
+        saw: {
+          level: number;
+        };
+        drill: {
+          level: number;
+        };
+      };
+    };
+    settings: {
+      sound: boolean;
+      music: boolean;
+      filter: string;
+      language: string;
+      brightness: number;
+    };
+  }
+  
