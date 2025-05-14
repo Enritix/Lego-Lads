4c7c33339ac029138c71bd56fd98d585c02a3c56
LarsVerheyden <lars.verheyden2000@gmail.com>
2025-05-14 08:59:00 +0000

    changed-detail and main css

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,87 +3,87 @@
 // }
 
 interface User {
-    _id: {
-      $oid: string;
-    };
-    username: string;
-    password: string;
-    email: string;
-    profile_fig: string;
-    coins: number;
-    earned_coins: number;
-    spent_coins: number;
-    figs: {
-      name: string;
-      img: string;
-      rarity: string;
-    }[];
-    bin: {
-      fig: string;
-      reason: string;
-    }[];
-    ordenedFigs: {
-      fig: string;
-      set: string;
-      theme: string;
-    }[];
-    achievements: {
-      coins: {
-        title: string;
-        description: string;
-        current: number;
-        goal: number;
-        reward: number;
-        finished: boolean;
-        collected: boolean;
-      };
-      login_streak: {
-        title: string;
-        description: string;
-        current: number;
-        goal: number;
-        reward: number;
-        finished: boolean;
-        collected: boolean;
-      };
-    };
-    chests: {
-      uncommon: number;
-      epic: number;
-      legendary: number;
-    };
-    keys: number;
-    clickerGame: {
-      blocks: number;
-      tools: {
-        hammer: {
-          level: number;
-        };
-        saw: {
-          level: number;
-        };
-        drill: {
-          level: number;
-        };
-      };
-    };
-    settings: {
-      sound: boolean;
-      music: boolean;
-      filter: string;
-      language: string;
-      brightness: number;
-    };
-  }
-  
-  export interface Minifig {
-    id: number;
+  _id: {
+    $oid: string;
+  };
+  username: string;
+  password: string;
+  email: string;
+  profile_fig: string;
+  coins: number;
+  earned_coins: number;
+  spent_coins: number;
+  figs: {
     name: string;
-    rarity: string;
     img: string;
+    rarity: string;
+  }[];
+  bin: {
+    fig: string;
+    reason: string;
+  }[];
+  ordenedFigs: {
+    fig: string;
+    set: string;
+    theme: string;
+  }[];
+  achievements: {
+    coins: {
+      title: string;
+      description: string;
+      current: number;
+      goal: number;
+      reward: number;
+      finished: boolean;
+      collected: boolean;
+    };
+    login_streak: {
+      title: string;
+      description: string;
+      current: number;
+      goal: number;
+      reward: number;
+      finished: boolean;
+      collected: boolean;
+    };
+  };
+  chests: {
+    uncommon: number;
+    epic: number;
+    legendary: number;
+  };
+  keys: number;
+  clickerGame: {
+    blocks: number;
+    tools: {
+      hammer: {
+        level: number;
+      };
+      saw: {
+        level: number;
+      };
+      drill: {
+        level: number;
+      };
+    };
+  };
+  settings: {
+    sound: boolean;
+    music: boolean;
+    filter: string;
+    language: string;
+    brightness: number;
+  };
+}
+
+export interface Minifig {
+  id: number;
+  name: string;
+  rarity: string;
+  img: string;
 }
 
 export interface binElement {
-    fig: string;
-    reason: string;
+  fig: string;
+  reason: string;
 }
